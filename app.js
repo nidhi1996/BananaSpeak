893afa9262ab95046f87fc7749494e8713cb41d4
Nidhi Soni <nidhisoni1796@gmail.com>
2022-08-11 21:45:38 +0530

    fetch done

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var txtOutput = document.querySelector("#txt-output p");
-btnTranslate.addEventListener("click", function clickeventhandler(){
-    txtOutput.innerHTML= txtInput.value;
-    
-});
+//server URL to minion API
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+//prepare server URL for querying [load]
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
+}
+//error handling
+function errorHandler(error){
+    console.log("Error occured ",error)
+}
+//click event
+function clickEventHandler() {
+    var inputText = txtInput.value;
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {txtOutput.innerText=json.contents.translated
+        })
+        .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickEventHandler);
