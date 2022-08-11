f2cc71f66d32e44b417478a00ad51f2f831f19ab
Nidhi Soni <nidhisoni1796@gmail.com>
2022-08-11 21:05:17 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output p");
 btnTranslate.addEventListener("click", function clickeventhandler(){
+    txtOutput.innerHTML= txtInput.value;
     
 });
